c0e4a472d8f487406eae9e01744d32342f82a943
DaMatrix <joey.rabil@gmail.com>
2017-11-26 13:55:46 +0100

    use pallette, and save localized names

--- src/main/java/net/daporkchop/bettermaps/grabber/GrabberMod.java
+++ src/main/java/net/daporkchop/bettermaps/grabber/GrabberMod.java
@@ -6,6 +6,7 @@ import com.google.gson.JsonObject;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockBed;
 import net.minecraft.block.material.MapColor;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
@@ -80,41 +81,59 @@ public class GrabberMod {
         blockMapColor.setAccessible(true);
 
         JsonObject object = new JsonObject();
-        JsonArray array = new JsonArray();
-        for (Block block : Block.REGISTRY) {
-            NonNullList<ItemStack> list = NonNullList.<ItemStack>create();
-            block.getSubBlocks(null, list);
-            for (ItemStack stack : list) {
-                JsonObject toAdd = new JsonObject();
-                Block block1 = Block.getBlockFromItem(stack.getItem());
-                toAdd.addProperty("registryName", block1.getRegistryName().toString());
-                toAdd.addProperty("id", Block.REGISTRY.getIDForObject(block1));
-                toAdd.addProperty("meta", stack.getMetadata());
-                if (!(block1 instanceof BlockBed)) {
-                    MapColor color = block1.getMapColor(Block.getBlockFromItem(stack.getItem()).getStateFromMeta(stack.getMetadata()), null, BlockPos.ORIGIN);
-                    toAdd.add("color", serializeMapColor(color));
+
+        {
+            JsonObject colorList = new JsonObject();
+            for (MapColor color : MapColor.COLORS) {
+                if (color == null) {
+                    continue;
                 }
-                array.add(toAdd);
+                colorList.add(String.valueOf(color.colorIndex), serializeMapColor(color, color.colorIndex));
             }
+            object.add("colors", colorList);
         }
-        object.add("colors", array);
+
+        {
+            JsonArray array = new JsonArray();
+            for (Block block : Block.REGISTRY) {
+                NonNullList<ItemStack> list = NonNullList.create();
+                block.getSubBlocks(null, list);
+                for (ItemStack stack : list) {
+                    JsonObject toAdd = new JsonObject();
+                    Block block1 = Block.getBlockFromItem(stack.getItem());
+                    if (block1 instanceof BlockBed) {
+                        //causes a nullpointer because it's retardedly coded
+                        continue;
+                    }
+                    IBlockState state = block1.getStateFromMeta(stack.getMetadata());
+                    toAdd.addProperty("registryName", block1.getRegistryName().toString());
+                    toAdd.addProperty("id", Block.REGISTRY.getIDForObject(block1));
+                    toAdd.addProperty("meta", stack.getMetadata());
+                    toAdd.addProperty("color", block1.isOpaqueCube(state) ? String.valueOf(block1.getMapColor(state, null, BlockPos.ORIGIN).colorIndex) : "0");
+                    toAdd.addProperty("localizedName", stack.getItem().getItemStackDisplayName(stack));
+                    array.add(toAdd);
+                }
+            }
+            object.add("blocks", array);
+        }
         object.addProperty("mcVersion", MinecraftForge.MC_VERSION);
+
         File file = new File(".", "bmaColors.json");
         if (file.exists()) {
             file.delete();
         }
         file.createNewFile();
         IOUtils.write(new GsonBuilder().setPrettyPrinting().create().toJson(object), new FileOutputStream(file));
-        logger.info("Done! Saved the colors of " + array.size() + " blocks!");
+        logger.info("Done! Saved to " + file.getAbsoluteFile().getAbsolutePath());
     }
 
-    public JsonObject serializeMapColor(MapColor color) {
+    public JsonObject serializeMapColor(MapColor color, int index) {
         Color color1 = new Color(color.colorValue);
         JsonObject object = new JsonObject();
         object.addProperty("red", color1.getRed());
         object.addProperty("green", color1.getGreen());
         object.addProperty("blue", color1.getBlue());
-        object.addProperty("alpha", color1.getAlpha());
+        object.addProperty("alpha", index == 0 ? 0 : color1.getAlpha());
         return object;
     }
 }
